e85344dd5274ab0f5af22be96e7b380246a52412
gabo899s <gaboleandro189@gmail.com>
2025-10-06 14:46:59 -0600

    Add App.vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <VisualBuilder />
+  </div>
+</template>
+
+<script>
+import VisualBuilder from './components/admin/VisualBuilder/VisualBuilder.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VisualBuilder
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  background: #F6EFD4;
+  font-family: Arial, sans-serif;
+}
+</style>
